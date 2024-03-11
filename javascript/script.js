7e5676d15ae324c240af1af885cb1e775409feea
LimKyungEun <gim970143@naver.com>
2024-03-11 16:14:04 +0900

    html 공지사항, 갤러리 추가

--- javascript/script.js
+++ javascript/script.js
@@ -10,9 +10,14 @@ jQuery(document).ready(function(){
         $(this).find('.submenu').stop().slideUp(500);
     });
 
-    //gt(0)은 index값보다 더 큰값을 가진 요소를 선택함
+    //gt(0)은 index값보다 더 큰값을 가진 요소를 선택함. 0번째부터 계수하여 index값보다 큰 값을 가져옴 
+    //여기에서 gt(0)이므로 1,2,3이 됨
     $('.imgslide a:gt(0)').hide();
     setInterval(function(){
-        $('.imgslide a:first-child').fadeOut().next('a').fadeIn().end.appendTo('.imgslide');},3000);
+        $('.imgslide a:first-child').fadeOut().//imgslide의 자식 요소<a>요소 중에서 첫번째를 선택하여 페이드 아웃 실행
+        next('a').fadeIn().//다음요소를 선택하여 페이드인 실행
+        end.//이전 선택 요소를 선택
+        appendTo('.imgslide')//선택한 요소를 .imgslide선택자의 요소로 추가
+        ;},3000);// setInterval(function(){},3000 = 일정시간마다 반복적으로 동작을 실행 3000은 3초에 해당 = 3초마다 실행
 });
 
