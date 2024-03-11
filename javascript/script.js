fc43d97dd9178e3d0e2562e61f19d8019787ff95
LimKyungEun <gim970143@naver.com>
2024-03-11 17:06:48 +0900

    JS 모달레이어 팝업기능 구현

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function(){
 
+    //메뉴열기
     $('.navi>li').mouseover(function() {//.navi>li에 mouseover와 mouseout이벤트 설정
         $(this).//this는 현재 선택된 요소
         find('.submenu').// 선택된 요소의 자식 요소중 .submenu와 일치하는 요소를 찾아 반환
@@ -10,14 +11,23 @@ jQuery(document).ready(function(){
         $(this).find('.submenu').stop().slideUp(500);
     });
 
+    //이미지 슬라이드
     //gt(0)은 index값보다 더 큰값을 가진 요소를 선택함. 0번째부터 계수하여 index값보다 큰 값을 가져옴 
-    //여기에서 gt(0)이므로 1,2,3이 됨
     $('.imgslide a:gt(0)').hide();
     setInterval(function(){
         $('.imgslide a:first-child').fadeOut().//imgslide의 자식 요소<a>요소 중에서 첫번째를 선택하여 페이드 아웃 실행
         next('a').fadeIn().//다음요소를 선택하여 페이드인 실행
-        end.//이전 선택 요소를 선택
-        appendTo('.imgslide')//선택한 요소를 .imgslide선택자의 요소로 추가
-        ;},3000);// setInterval(function(){},3000 = 일정시간마다 반복적으로 동작을 실행 3000은 3초에 해당 = 3초마다 실행
+        end().//이전 선택 요소를 선택
+        appendTo('.imgslide');}//선택한 요소를 .imgslide선택자의 요소로 추가
+        ,3000);// setInterval(function(){},3000 = 일정시간마다 반복적으로 동작을 실행 3000은 3초에 해당 = 3초마다 실행
+
+
+    //팝업기능
+    $('.notice li:first').click(function(){
+        $('#modal').addClass('active');
+    });
+    $('.btn').click(function(){
+        $('#model').removeClass('active');
+    })
 });
 
