abbc883507627d0a74e2d2cebc01d552a054fed6
LimKyungEun <gim970143@naver.com>
2024-03-11 12:09:23 +0900

    환경설정완료

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+//JavavScript Document
+
+
+    $(".list>li").mouseover(function(){
+        var submenu = $(this).children(".submenu").slideDown;});
+    
+    $(".list>li").mouseleave(function(){
+        $(this).children(".submenu").stop().slideUp();});
+
+
+  
+    
+
+    var win;
+    function winOpen(){
+    win = window.open('contact.html', 'child', 'toolbar=no, location=no, status=no, menubar=no, resize=no, scrollbars=no, width=500, height=300')};
+    
